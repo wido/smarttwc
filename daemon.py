28e3b453511aa0bfbdd124789c04cf63630eb47e
Wido den Hollander <wido@widodh.nl>
2017-12-12 22:51:20 +0100

    Work on actually getting a thread running
    Signed-off-by: Wido den Hollander <wido@widodh.nl>

--- daemon.py
+++ daemon.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 import config
+from threading import Event
 from smarttwc import Master
 
-master = Master(config.SERIAL_DEVICE, config.SERIAL_BAUD_RATE, config.MAX_CURRENT)
+master = Master(config.SERIAL_DEVICE,
+                config.SERIAL_BAUD_RATE,
+                config.MAX_CURRENT)
+master.daemon = True
+master.start()
+
+event = Event()
+while master.is_alive():
+    try:
+        event.wait(0.1)
+    except:
+        master.join()
--- smarttwc/master.py
+++ smarttwc/master.py
@@ -1,5 +1,28 @@
 import threading
+import serial
+import logging
+
 
 class Master(threading.Thread):
+    alive = True
+    interval = 0.1
+
     def __init__(self, serial_device, serial_baud_rate, max_current):
-        pass
+        super(Master, self).__init__()
+        self.event = threading.Event()
+        self.alive = True
+        self.serial_device = serial_device
+        self.serial_baud_rate = serial_baud_rate
+        self.max_current = max_current
+        self.dev = serial.Serial(serial_device, serial_baud_rate)
+
+    def run(self):
+        logging.info('Master thread starting up')
+        while self.alive:
+            logging.warning(self.dev.port)
+            self.event.wait(self.interval)
+
+    def join(self, timeout=None):
+        self.alive = False
+        self.dev.close()
+        super(Master, self).join(timeout)
