b4b3e3ce2ab38520c9898774ce6e8bee1c1b74c4
Wido den Hollander <wido@widodh.nl>
2017-12-13 09:40:35 +0100

    Add arguments for logging
    Signed-off-by: Wido den Hollander <wido@widodh.nl>

--- daemon.py
+++ daemon.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 import config
 import logging
+import argparse
 from threading import Event
 from smarttwc import Master
 
-logger = logging.basicConfig(level=logging.INFO)
+parser = argparse.ArgumentParser(description='SmartTWC')
+parser.add_argument("--debug", action="store_true", dest="debug",
+                    default=False, help="Debug logging")
+args = parser.parse_args()
+
+loglevel = logging.WARN
+if args.debug:
+    loglevel = logging.DEBUG
+
+logger = logging.basicConfig(level=loglevel)
 
 master = Master(config.SERIAL_DEVICE,
                 config.SERIAL_BAUD_RATE,
